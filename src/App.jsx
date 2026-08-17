e25bc3590a7c5d5d04906608d1c86afd5dcadc45
Mouhamed <m3dev4@gmail.com>
2026-08-17 15:22:03 +0000

    feat: implement App root component and navigation header layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Acceuil from './pages/acceuil'
 
 const App = () => {
-  const { isAuthenticated, user, logout } = useAuth()
+  // const { isAuthenticated, user, logout } = useAuth()
 
-useEffect(() => {
-console.log(user?.last_name)
-}, [user])
+// useEffect(() => {
+// console.log(user?.last_name)
+// }, [user])
 
   return (
     <div>
